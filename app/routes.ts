3885a0215bcff062d745dd78cf51de2ca184f059
Sumit Singh Karki <sumitkarki1114@gmail.com>
2025-10-07 11:10:03 +0530

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import {type RouteConfig, index, route} from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route('/auth', 'routes/auth.tsx'),
+    route('/auth', 'routes/Auth.tsx'),
     route('/upload', 'routes/upload.tsx'),
     route('/resume/:id', 'routes/resume.tsx'),
     route('/wipe', 'routes/wipe.tsx'),
